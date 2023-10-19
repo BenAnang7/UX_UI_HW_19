c35ac2ddfaa79a7ace614cca1c8e748c1bb9e137
BenAnang7 <133083379+BenAnang7@users.noreply.github.com>
2023-10-18 23:19:50 -0700

    2nd commit

--- js/index.js
+++ js/index.js
@@ -2,36 +2,49 @@ console.log("Your index.js file is loaded correctly!");
 $(document).ready(function () {
     $(".workItem").hover(
       function () {
-        // Mouse enters the element (hover effect)
+       
         $(this).css("background-color", "white");
       },
       function () {
-        // Mouse leaves the element (revert to the original background color)
-        $(this).css("background-color", ""); // Revert to the original background color
+       
+        $(this).css("background-color", ""); 
       }
     );
   });
   $("ul li").hover(
     function () {
-      // Mouse enters the element (hover effect)
+   
       $(this).css("text-decoration", "underline");
       $(this).css("text-shadow", "2px 2px 4px rgba(0, 0, 0, 0.2)"); 
     },
     function () {
-      // Mouse leaves the element (revert to the original style)
+     
       $(this).css("text-decoration", "none");
       $(this).css("box-shadow", "none");
     });
     $("button:contains('Resume')").hover(
         function () {
-          // Mouse enters the element (hover effect)
+          
           $(this).css("background-color", "red");
           $(this).css("color", "white");
         },
         function () {
-          // Mouse leaves the element (revert to the original style)
+         
           $(this).css("background-color", "");
           $(this).css("color", "");
         }
       );
+      $("button:contains('View Project')").hover(
+        function () {
+        
+          $(this).css("background-color", "red");
+          $(this).css("color", "white");
+        },
+        function () {
+         
+          $(this).css("background-color", "");
+          $(this).css("color", "");
+        }
+      );
+ 
   
